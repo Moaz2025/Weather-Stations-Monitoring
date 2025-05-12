d88390d0f3093b82840247f314d5e40d41282296
Moaz2025 <moaz20ahmed@gmail.com>
2025-05-12 23:17:27 +0300

    Initial files

---
+++ src/main/java/com/example/Weather_Stations_Monitoring/WeatherStationsMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Weather_Stations_Monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStationsMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStationsMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Weather_Stations_Monitoring/WeatherStationsMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Weather_Stations_Monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationsMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
